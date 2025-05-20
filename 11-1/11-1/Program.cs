8e4496f7b7eb17feeb26170d74e6bb87a6f9111a
翁誌璟 <s0968813171@gmail.com>
2025-05-20 12:45:06 +0800

    Add files via upload

---
+++ 11-1/11-1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Math1
+{
+    public static int 平方(int 整數)
+    {
+        return 整數 * 整數;
+    }
+    public static double 平方(double 小數)
+    {
+        return 小數 * 小數;
+    }
+    public static int 最小值(int a, int b, int c)
+    {
+        return Math.Min(a, Math.Min(b, c));
+    }
+    public static int 最小值(int a, int b, int c, int d)
+    {
+        return Math.Min(a, Math.Min(b, Math.Min(c, d)));
+    }
+}
+
+class 程式
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("整數 4 的平方: " + Math1.平方(4));
+        Console.WriteLine("小數 2.5 的平方: " + Math1.平方(2.5));
+        Console.WriteLine("三個數中的最小值 (7, 2, 5): " + Math1.最小值(7, 2, 5));
+        Console.WriteLine("四個數中的最小值 (9, 4, 6, 1): " + Math1.最小值(9, 4, 6, 1));
+    }
+}
